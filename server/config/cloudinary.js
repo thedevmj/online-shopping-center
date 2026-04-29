8d6c98dbc34ea34f5d8affe7738f038495a231df
Meow5166 <junaidmansuri71@gmail.com>
2026-04-29 22:07:29 +0530

    *

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,15 +1,16 @@
 const cloudinary=require("cloudinary").v2;
 
+import dotenv from "dotenv";
+
+dotenv.config();
 
 cloudinary.config({
-   cloud_name:'dnjlq7ule',
-   api_key:'372677654413622',
-   api_secret:'wdzkZEyt7YnMxEjtCCqudJCCZLA'
+   cloud_name:process.env.CLOUD_NAME,
+   api_key:process.env.CLOUD_API_KEY,
+   api_secret:process.env.CLOUD_API_SECRET
 
 })
-console.log(process.env.CLOUD_NAME);
-console.log(process.env.CLOUD_API_KEY);
-console.log(process.env.CLOUD_API_SECRET);
+
 
 
 module.exports={cloudinary};
